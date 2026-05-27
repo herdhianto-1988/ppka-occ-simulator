0c1b58e53b30e73110f92c1748f8d786b7f9bd98
Herdhianto <herdhiantonurma@gmail.com>
2026-05-27 18:30:33 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 import { renderMap } from "./ui/renderer.js";
 
-renderMap();
+let mapData;
+
+async function loadData() {
+
+  const response =
+    await fetch("./data/map.json");
+
+  mapData = await response.json();
+
+  gameLoop();
+}
+
+function gameLoop() {
+
+  updateTrains();
+
+  renderMap(mapData);
+
+  requestAnimationFrame(gameLoop);
+}
+
+function updateTrains() {
+
+  mapData.trains.forEach(train => {
+
+    train.x += train.speed;
+
+  });
+}
+
+loadData();
