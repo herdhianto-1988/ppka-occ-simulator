d3760649ef203e1bf3913cede411491d6ec77c7b
Herdhianto <herdhiantonurma@gmail.com>
2026-05-27 18:26:45 +0700

    Update renderer.js

--- js/ui/renderer.js
+++ js/ui/renderer.js
@@ -1,28 +1,65 @@
-export function renderMap() {
+export async function renderMap() {
+
+  const response = await fetch("./data/map.json");
+
+  const mapData = await response.json();
 
   const svg = document.getElementById("railway-map");
 
-  svg.innerHTML = `
-  
-    <line 
-      x1="100" 
-      y1="200" 
-      x2="700" 
-      y2="200"
-      stroke="white"
-      stroke-width="6"
-    />
+  svg.innerHTML = "";
 
-    <circle cx="250" cy="200" r="10" fill="red" />
-    <circle cx="550" cy="200" r="10" fill="red" />
+  // Render track
+  mapData.tracks.forEach(track => {
 
-    <text x="90" y="180" fill="white">
-      STA A
-    </text>
+    const fromStation =
+      mapData.stations.find(s => s.id === track.from);
 
-    <text x="690" y="180" fill="white">
-      STA B
-    </text>
+    const toStation =
+      mapData.stations.find(s => s.id === track.to);
 
-  `;
+    svg.innerHTML += `
+      <line
+        x1="${fromStation.x}"
+        y1="${fromStation.y}"
+        x2="${toStation.x}"
+        y2="${toStation.y}"
+        stroke="white"
+        stroke-width="6"
+      />
+    `;
+  });
+
+  // Render stations
+  mapData.stations.forEach(station => {
+
+    svg.innerHTML += `
+      <text
+        x="${station.x - 20}"
+        y="${station.y - 20}"
+        fill="white"
+      >
+        ${station.name}
+      </text>
+    `;
+  });
+
+  // Render signals
+  mapData.signals.forEach(signal => {
+
+    let color = "red";
+
+    if(signal.aspect === "green") {
+      color = "lime";
+    }
+
+    svg.innerHTML += `
+      <circle
+        cx="${signal.x}"
+        cy="${signal.y}"
+        r="10"
+        fill="${color}"
+      />
+    `;
+  });
+
 }
